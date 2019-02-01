c8be92d0e09e3b2b985f180d39ae7773a6ad67c1
TOYefule <Tolani.Oyefule@gmail.com>
2019-02-01 15:56:46 -0500

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+//Challenge 2
+public class Main {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        Scanner sc = new Scanner();
+
+        // Variables:git pus
+        int money = 0;
+        int betAmount = 0;
+        int gamesWon = 0;
+        int gamesLost = 0;
+        int num1 = 1 +r.nextInt(6);
+        int num2 = 1 +r.nextInt(6);
+//          String:
+//        String high = variable high;
+//        String low = variable low;
+//        String red = 1 +r red;
+//        String black = variable black;
+
+        System.out.println(" WELCOME! Let's play some Roulette!");
+        System.out.println("Enter the type of bet you would like to place (red|black|even|odd|high|low): ");
+        System.in(red);
+	if(Argument){ /*Do stuff*/};
+
+
+System.out.println("Enter in your bet amount: ");
+	System.in(35);
+System.out.println(" The ball landed on " /*random.int(27) + random.color(black) + */".");
+if(state.col == true && state.num == true) {
+	System.out.println("Congratulations, you've won.");
+} else {
+    System.out.println("Sorry, you've lost this bet.");
+
+// You currently have: $-35.0
+    system.out.println("You currently have: $"money(-35.0));
+    system.out.println("Would you like to play again (true|false)?");
+    System.in(true);
+/*
+Enter the type of bet you would like to place (red|black|even|odd|high|low): low
+Enter in your bet amount: 40
+The ball landed on 17 black.
+Congratulations, you've won.
+You currently have: $45.0
+Would you like to play again (true|false)? true
+Enter the type of bet you would like to place (red|black|even|odd|high|low): even
+Enter in your bet amount: 100
+The ball landed on 29 black.
+Sorry, you've lost this bet.
+You currently have: $-55.0
+Would you like to play again (true|false)? false
+*/
+
+    /*
+        System.out.println("Thank you for playing!")
+        System.out.println("This is how much money you have remaining: $" + money(-55.00))
+        System.out.println("You've won " + gameswon +" game(s) and lost " +gameslost+ " game(s).")
+        }
+
+    */
+}}
+}
